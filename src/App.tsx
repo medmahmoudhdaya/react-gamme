520389b18a9f23bb0fa45c13daa38bf71497c93b
zidbih <medmahmoudhdaya@gmail.com>
2025-12-03 20:09:43 +0000

    adding the correct word for more better UX

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,10 @@ export default function App() {
   return (
       <Container>
         <div className="w-full mb-2 shadow mx-auto">
-          <p className="text-xl text-blue-500 text-center py-2 font-bold">you have {words.filter(e=>e=='').length} tries left.</p>
+          <p className="text-xl text-blue-500 text-center py-2 font-bold">
+            {words.filter(e=>e=='').length === 0 ? words[5] === solution ? "You are great !" : "The correct word was : "+solution
+            : ("you have" + words.filter(e=>e=='').length + "tries left.")}
+          </p>
         </div>
         <div className="mx-auto flex flex-col gap-1">
         {
